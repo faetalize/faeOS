b9c39b1e16ec59dbadc955b54b7969a88a3b7969
faetalize <faetalize@pm.me>
2025-01-04 16:04:50 +0100

    refactor: enhance DesktopEnvironment component with active window handling and mouse interaction

--- src/Components/Desktop Environment/DesktopEnvironment.tsx
+++ src/Components/Desktop Environment/DesktopEnvironment.tsx
@@ -1,5 +1,4 @@
-
-import { ReactNode, useState } from 'react';
+import { ReactNode, useState, useEffect, MouseEvent, useRef } from 'react';
 import './style.css';
 import Toolbar from './Toolbar';
 import Dock from './Dock';
@@ -9,15 +8,36 @@ interface DEProps {
 }
 
 const DesktopEnvironment = ({ children }: DEProps) => {
-    const [active, setActive] = useState(0);
-    
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+    const desktopEnvironment = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (activeIndex !== null && desktopEnvironment.current) {
+            const windows = desktopEnvironment.current.querySelectorAll('.window');
+            windows.forEach((window, index) => {
+                window.classList.toggle('active', index === activeIndex);
+            });
+        }
+    }, [activeIndex])
+
+    const handleMouseDown = (event: MouseEvent) => {
+        const windows = desktopEnvironment.current!.querySelectorAll('.window');
+        windows?.forEach((window, index) => {
+            if (window.contains(event.target as HTMLDivElement)) {
+                setActiveIndex(index);
+            }
+        });
+    }
+
     return (
-        <div className="desktop-environment">
-            <Toolbar></Toolbar>
-            {children}
-            <Dock></Dock>
+        <div className="desktop-environment" onMouseDown={handleMouseDown} ref={desktopEnvironment}>
+            <Toolbar />
+            <div className="de-window-container">
+                {children}
+            </div>
+            <Dock />
         </div>
     );
 }
 
-export default DesktopEnvironment
+export default DesktopEnvironment;
