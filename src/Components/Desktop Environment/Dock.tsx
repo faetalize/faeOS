3f85123a33a0e7532b710537f9f7124454c56d10
faetalize <faetalize@pm.me>
2025-01-22 12:02:07 +0100

    Add thumbnail display to Dock component and enhance app icon highlighting

--- src/Components/Desktop Environment/Dock.tsx
+++ src/Components/Desktop Environment/Dock.tsx
@@ -3,16 +3,23 @@ import { allAppsContext } from '../../Contexts/allApps';
 import styles from './style.module.css';
 import { runningProcessesContext } from '../../Contexts/runningProcesses';
 
-
 interface DockProps {
     onEntryClick: (appName: string) => void,
-    activeIndex: number | null
+    activeIndex: number | null,
+    thumbs: string[]
 }
 
-
-const Dock = forwardRef<HTMLDivElement,DockProps>(({ onEntryClick, activeIndex }: DockProps, ref) => {
+const Dock = forwardRef<HTMLDivElement, DockProps>(({ onEntryClick, activeIndex, thumbs}: DockProps, ref) => {
     const allApps = useContext(allAppsContext);
     const runningProcesses = useContext(runningProcessesContext);
+
+    const isAppRunning = (appName: string) => {
+        return runningProcesses.processes.some(process => process.app.appName === appName);
+    };
+
+    const isAppFocused = (appName: string) => {
+        return runningProcesses.processes.some(process => process.app.appName === appName && process.id === activeIndex);
+    };
 
     return (
         <div className={styles['dock']} ref={ref}>
@@ -20,21 +27,26 @@ const Dock = forwardRef<HTMLDivElement,DockProps>(({ onEntryClick, activeIndex }
                 <img src="/launchpad.webp" alt="icon" />
             </div>
             {
-                allApps?.map(app => (
-                    <div key={app.appName} className={styles['icon']} onClick={() => onEntryClick(app.appName)}>
+                allApps?.map((app) => (
+                    <div
+                        key={app.appName}
+                        className={`${styles['icon']} ${isAppRunning(app.appName) ? styles['icon-highlighted'] : ''} ${isAppFocused(app.appName) ? styles['icon-focused'] : ''}`}
+                        onClick={() => onEntryClick(app.appName)}
+                    >
                         <img src={app.icon} alt={app.appName} />
+                        <div className={styles['icon-indicator']}></div>
                     </div>
                 ))
             }
             {
-                runningProcesses.processes?.map(process => (
-                    <div key={process.id} className={`${styles['icon']} ${activeIndex === process.id ? styles['icon-highlighted'] : "" }`}>
-                        <img src={process.app.icon} alt={process.app.appName} />
+                thumbs.map((thumb, index) => (
+                    <div key={index} className={styles['icon']}>
+                        <img src={thumb} alt="thumb" />
                     </div>
                 ))
             }
         </div>
-    )
+    );
 });
 
 export default Dock;
